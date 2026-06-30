71127628e13bdc69dae1201829a42a75a2a70f56
Rafael Ruhoff Gonçalves casas <131457092+ngt18@users.noreply.github.com>
2026-06-30 19:42:47 -0300

    Criação do der+script sql

---
+++ db.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE biblioteca;
+
+USE biblioteca;
+
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE livros (
+    id_livro INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    disponivel BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE reservas (
+    id_reserva INT AUTO_INCREMENT PRIMARY KEY,
+    data_reserva DATE NOT NULL,
+    status VARCHAR(30) DEFAULT 'ATIVA',
+    id_usuario INT NOT NULL,
+    id_livro INT NOT NULL,
+
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (id_livro) REFERENCES livros(id_livro)
+);
